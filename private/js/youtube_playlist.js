cfb62efc80f2ecb7fb10453569a9d989f3bbffb1
InfiniteWing <sars6608@gmail.com>
2020-09-05 19:03:09 +0800

    Hello World!!!

---
+++ private/js/youtube_playlist.js
@@ -0,0 +1,33 @@
+$(function () {
+	var videos = [
+		{videoURL: "-P3squTEJ4M",containment:'self',autoPlay:false, startAt:0,opacity:1, loop:true, stopMovieOnBlur: false, remember_last_time: true},
+		{videoURL:"HQnC1UHBvWA",containment:'self',autoPlay:false, startAt:0,opacity:1, loop:true, stopMovieOnBlur: false, remember_last_time: true},
+		{videoURL: "VcQIpQB4Fiw",containment:'self',autoPlay:false, startAt:0,opacity:1, loop:true, stopMovieOnBlur: false, remember_last_time: true}
+	];
+	$(".yt_player").YTPlaylist(videos, false, function(video){});
+	$(".yt_player").on("YTPPlay",function(e){
+		$(".youtube-audio-play-control").removeClass("fa-play").addClass("fa-pause");
+	});	
+	$(".yt_player").on("YTPPause",function(e){
+		$(".youtube-audio-play-control").removeClass("fa-pause").addClass("fa-play");
+	});	
+	$(".youtube-audio-play-control").click(function () {
+        if ($(".youtube-audio-play-control").hasClass("fa-play")) {
+            $(".yt_player").YTPPlay();
+        }
+        else {
+            
+            $(".yt_player").YTPPause();
+        }
+    });
+	$(".youtube-audio-forward").click(function () {
+		$(".yt_player").YTPPlayNext();
+		return false;
+	});
+	$(".youtube-audio-forward").click(function () {
+		$(".yt_player").YTPPlayPrev();
+		return false;
+	});
+})(jQuery);
+
+
